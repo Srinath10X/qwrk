f1d2b1b914a6c394a10a287a734be2d857b18fe5
Srinath10X <srinathcreates@gmail.com>
2025-07-20 02:36:08 +0530

    feat(types): add types for qwrk-vite

--- packages/qwrk-vite/index.d.ts
+++ packages/qwrk-vite/index.d.ts
@@ -1,1 +1,14 @@
-declare module "qwrk-vite";
+declare module "qwrk" {
+  interface Plugin {
+    name: string;
+    config?: () => {
+      esbuild?: {
+        jsxFactory?: string;
+        jsxFragment?: string;
+        jsxInject?: string;
+      };
+    };
+  }
+
+  export default function qwrk(): Plugin;
+}
