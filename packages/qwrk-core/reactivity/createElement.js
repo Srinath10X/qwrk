1c4f9ab56095a8ab42482e7a7554d70616e7fc04
Srinath10X <srinathcreates@gmail.com>
2025-09-19 13:00:31 +0530

    feat(core): implemented attributes converting logic

--- packages/qwrk-core/reactivity/createElement.js
+++ packages/qwrk-core/reactivity/createElement.js
@@ -1,6 +1,13 @@
 import { instanceOf, fragment } from "../core/utils.js";
 import { setAttributes, _processedChildren } from "./internals.js";
 
+/**
+ * @param {string | Function | any} tag
+ * @param {Record<string, any>} attributes
+ * @param {...any} children
+ *
+ * @returns {*}
+ */
 export function createElement(tag, attributes, ...children) {
   if (tag === fragment) return _processedChildren(children);
   if (instanceOf(tag, Function)) return tag({ children, ...attributes });
@@ -9,7 +16,19 @@ export function createElement(tag, attributes, ...children) {
 
   for (let key in attributes) {
     const value = attributes[key];
-    key = key === "className" ? "class" : key.toLowerCase();
+
+    ///  TODO: Clean up this bullshit below
+
+    switch (key) {
+      case "className":
+        key = "class";
+
+      case "htmlFor":
+        key = "for";
+    }
+
+    key = key.toLowerCase();
+
     key.substring(0, 2) == "on"
       ? element.addEventListener(key.slice(2), value)
       : setAttributes(element, key, value);
